e147ae09f62a99282babc600b13bbe83a0c45985
mungilwar <nitinmungilwar4@gmail.com>
2023-02-17 17:59:42 +0530

    completed

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ErrorBoundary = () => {
+  return <div>hi there is something wrong..</div>;
+};
+
+export default ErrorBoundary;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+// import ErrorBoundary from './components/ErrorBoundary';
+ReactDOM.render(
+  //   <ErrorBoundary>
+  <App />,
+  //   </ErrorBoundary>,
+  document.getElementById('root')
+);
